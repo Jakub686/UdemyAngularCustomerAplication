8763e60b8b3e6567568046a6c4a1b3b2781b9a41
Jakub <jakub.bchyla@gmail.com>
2022-12-09 10:18:47 +0100

    lab2 00:09

---
+++ src/CustomerApp/CustomerApp.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Customer } from "./CutomerApp.model";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './Customerapp.view.html',
+
+})
+export class CustomerComponent {
+  title = 'CustomerAplication';
+}
--- src/CustomerApp/CustomerApp.module.ts
+++ src/CustomerApp/CustomerApp.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+
+import { CustomerComponent } from './CustomerApp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    CustomerComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CustomerComponent]
 })
 export class AppModule { }
